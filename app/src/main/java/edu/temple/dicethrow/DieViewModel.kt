f1aa398f0fa5ff07f0d5a14136091c9a8bb768b3
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-10-24 14:28:27 -0400

    created getDieRoll() method

--- app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
+++ app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
@@ -1,6 +1,14 @@
 package edu.temple.dicethrow
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DieViewModel : ViewModel {
+class DieViewModel : ViewModel() {
+    private val dieRoll = MutableLiveData<Int>()
+
+    fun getDieRoll(): LiveData<Int> {
+        return dieRoll
+    }
+//    fun setDieRoll() {}
 }
